5e4e02ab5ca548841c68ff267cc6abf4f3e064fe
Mtantana <mantana.62107594@gmail.com>
2023-01-16 15:47:21 +0700

    add button home page

--- views/site/home.php
+++ views/site/home.php
@@ -4,4 +4,17 @@
     $this->title = 'Home';
 ?>
 
-<H1>Hello Home Page</H1>
+<!doctype html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+<h2>Setting the Viewport</h2>
+<p>This example does not really do anything, other than showing you how to add the viewport meta element.</p>
+
+
+<button type="button" > เข้าสู่ระบบ </button>
+</body>
+</html>
