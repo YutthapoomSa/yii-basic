a97b28ab80a87919559a88c116dd89605201c5a2
YutthapoomSa <wavee03@gmail.com>
2023-01-17 14:09:27 +0700

    update

---
+++ config/sub-menu.php
@@ -0,0 +1,62 @@
+<?php
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+/**
+ * Application configuration shared by all test types
+ */
+return [
+    'id' => 'basic-sub-menu',
+    'basePath' => dirname(__DIR__),
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'language' => 'en-US',
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'fnVo6He_LD_RBeKiNeAymFvL8FrUVchB',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'sub-menu' => [
+            'identityClass' => 'app\models\SubMenu',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
---
+++ views/sub-menu/dashboard.php
@@ -0,0 +1,27 @@
+<?php
+$this->title = 'Dashboard';
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Dashboard Sub Menu</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- <link rel="stylesheet" href="css/loginHome.css"> -->
+</head>
+// ─────────────────────────────────────────────────────────────────────────────
+
+<body class="img js-fullheight height: 100px; background-position: center; background-repeat: no-repeat; background-size: cover;" >
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Testtt</div>
+                </div>
