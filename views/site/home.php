aa1ebd34f06cbff48907daf3576540f462962539
Mtantana <mantana.62107594@gmail.com>
2023-01-17 16:34:55 +0700

    add and do home page, home css

--- views/site/home.php
+++ views/site/home.php
@@ -8,13 +8,23 @@
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&family=Limelight&family=Noto+Sans+Thai:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/home.css">
 
 </head>
-<body>
-<h2>Setting the Viewport</h2>
-<p>This example does not really do anything, other than showing you how to add the viewport meta element.</p>
+<body class="img js-fullheight" style="background-image: url(images/coverhome.png); height: 100px; background-position: center; background-repeat: no-repeat; background-size: cover; object-fit:cover;">
+
+      <div class="col-4 col-s-8">
+
+        <h1 class="header">Smart <br>Dashboard</h1>
+    
+        <p>ระบบแดชบอร์ดอัจฉริยะเป็นระบบหน้าจอที่สรุปข้อมูลทุกอย่างให้อยู่ในหน้าจอเดียวเพื่อให้ผู้บริหารดูข้อมูลแล้วสามารถเข้าใจได้ทันที</p>
+      
+        <div style="text-align:center;"><a href="index.php?r=site%2Flog" target=_blank><button class="buttonhome button2" role="button"> เข้าสู่ระบบ </button></a></div>
+      </div>
 
 
-<button type="button" > เข้าสู่ระบบ </button>
 </body>
 </html>
